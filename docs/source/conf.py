4f3363736dada1d29f01cd3e4fe412546e1b5b58
raam93 <raam.arvind93@gmail.com>
2019-06-19 15:02:47 +0530

    checking docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,18 +18,18 @@ sys.path.insert(0, os.path.abspath("../../"))
 # -- Project information -----------------------------------------------------
 
 project = 'DiCE'
-copyright = '2019, Ramaravind, Amit, Chenhao'
-author = 'Ramaravind, Amit, Chenhao'
+copyright = '2019, Ramaravind, Amit Sharma, Chenhao'
+author = 'Ramaravind, Amit Sharma, Chenhao'
 
 # The full version, including alpha/beta/rc tags
 release = '2.0.6'
+
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
 extensions = ['sphinx.ext.autodoc', 
     'sphinx.ext.viewcode',
     'sphinx.ext.todo'
@@ -51,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. DiCE documentation master file, created by
-   sphinx-quickstart on Wed Jun 19 14:04:31 2019.
+   sphinx-quickstart on Wed Jun 19 14:47:07 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
