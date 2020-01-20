57d2ed90173eda8578d298a48d073d8a142aaac0
divyat09 <divyatmahajan@gmail.com>
2020-01-20 19:15:31 +0530

    Pytorch model interface added and training exmaple for Adult included in notebool

--- dice_ml/model_interfaces/pytorch_model.py
+++ dice_ml/model_interfaces/pytorch_model.py
@@ -1,16 +1,36 @@
 """Module containing an interface to trained PyTorch model (Future Support)."""
 
-class PyTorchModel:
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from torch.autograd import Variable
 
-    def __init__(self, model):
+class PyTorchModel(nn.Module):
+
+    def __init__(self, inp_shape):
         """Init method
 
         :param model: trained PyTorch Model.
 
         """
 
-        self.model = model
-
+        super(PyTorchModel, self).__init__()
+        self.inp_shape= inp_shape
+        self.hidden_dim= 20
+        self.num_classes=2
+        self.ann_model= nn.Sequential(
+                        nn.Linear( self.inp_shape, self.hidden_dim),
+                        nn.ReLU(),
+                        nn.Linear( self.hidden_dim, self.num_classes),
+                        nn.Softmax(),
+                )
+    
+    def forward(self, x):
+        return self.ann_model(x)
+       
     def get_output(self, input):
         # Future Support
         return None
