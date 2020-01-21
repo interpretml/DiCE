39ce7a6cf333ec3826b0e8a538b03845b47b5ce8
divyat09 <divyatmahajan@gmail.com>
2020-01-21 21:52:56 +0530

    Outcome Variable Positioning error after one hot encoding correct (Adult dataset) and BaseGenCF method train function working code added

--- dice_ml/dice_interfaces/dice_base_gencf.py
+++ dice_ml/dice_interfaces/dice_base_gencf.py
@@ -32,18 +32,11 @@ class DiceBaseGenCF:
 
         # Dataset for training Variational Encoder Decoder model for CF Generation
         train_data_vae= self.data_interface.data_df.copy()
-        # Can remove the condition of training on only low income dataset
-        train_data_vae= train_data_vae[ train_data_vae['income']==0 ]
-
+        
         #MAD
         #self.mad_feature_weights = self.data_interface.get_mads_from_training_data(normalized=False)
 
-        #One Hot Encoding for categorical features
-        encoded_data = self.data_interface.one_hot_encode_data(train_data_vae)
-        dataset = encoded_data.to_numpy()
-
-        #Normlaise_Weights
-        self.normalise_weights={}
+        #Creating list of encoded categorical and continuous feature indices
         encoded_categorical_feature_indexes = self.data_interface.get_data_params()[2]     
         encoded_continuous_feature_indexes=[]
         for i in range(self.data_size):
@@ -54,12 +47,25 @@ class DiceBaseGenCF:
             if valid:
                 encoded_continuous_feature_indexes.append(i)            
         encoded_start_cat = len(encoded_continuous_feature_indexes)
+        
+        #One Hot Encoding for categorical features
+        encoded_data = self.data_interface.one_hot_encode_data(train_data_vae)
+        
+        # The output/outcome variable position altered due to one_hot_encoding for categorical features: (Cont feat, Outcome, Cat feat) 
+        # Need to rearrange columns such that outcome variable comes at the last
+        cols = list(encoded_data.columns)
+        cols = cols[:encoded_start_cat] + cols[encoded_start_cat+1:] + [cols[encoded_start_cat]]
+        encoded_data = encoded_data[cols]        
+
+        #Normlaise_Weights
+        self.normalise_weights={}        
+        dataset = encoded_data.to_numpy()
         for idx in encoded_continuous_feature_indexes:
             _max= float(np.max( dataset[:,idx] ))
             _min= float(np.min( dataset[:,idx] ))
             self.normalise_weights[idx]=[_min, _max]
 
-        #Normlization for conitnuous features
+        # Normlization for continuous features
         encoded_data= self.data_interface.normalize_data(encoded_data)
         dataset = encoded_data.to_numpy()
 
@@ -94,82 +100,91 @@ class DiceBaseGenCF:
             ], lr=self.learning_rate
         )
     
-        def compute_loss( self, model_out, x, target_label ): 
+    def compute_loss( self, model_out, x, target_label ): 
 
-            em = model_out['em']
-            ev = model_out['ev']
-            z  = model_out['z']
-            dm = model_out['x_pred']
-            mc_samples = model_out['mc_samples']
-            #KL Divergence
-            kl_divergence = 0.5*torch.mean( em**2 +ev - torch.log(ev) - 1, axis=1 ) 
+        em = model_out['em']
+        ev = model_out['ev']
+        z  = model_out['z']
+        dm = model_out['x_pred']
+        mc_samples = model_out['mc_samples']
+        #KL Divergence
+        kl_divergence = 0.5*torch.mean( em**2 +ev - torch.log(ev) - 1, axis=1 ) 
 
-            #Reconstruction Term
-            #Proximity: L1 Loss
-            x_pred = dm[0]       
-            s= self.cf_vae.encoded_start_cat
-            recon_err = -torch.sum( torch.abs(x[:,s:-1] - x_pred[:,s:-1]), axis=1 )
+        #Reconstruction Term
+        #Proximity: L1 Loss
+        x_pred = dm[0]       
+        s= self.cf_vae.encoded_start_cat
+        recon_err = -torch.sum( torch.abs(x[:,s:-1] - x_pred[:,s:-1]), axis=1 )
+        for key in self.normalise_weights.keys():
+            # recon_err+= -(1/mad_feature_weights[d.encoded_feature_names[int(key)]])*(normalise_weights[key][1] - normalise_weights[key][0])*torch.abs(x[:,key] - x_pred[:,key]) 
+            recon_err+= -(self.normalise_weights[key][1] - self.normalise_weights[key][0])*torch.abs(x[:,key] - x_pred[:,key]) 
+
+        # Sum to 1 over the categorical indexes of a feature
+        for v in self.cf_vae.encoded_categorical_feature_indexes:
+            temp = -torch.abs(  1.0-torch.sum( x_pred[:, v[0]:v[-1]+1], axis=1) )
+            recon_err += temp
+
+        count=0
+        count+= torch.sum(x_pred[:,:s]<0,axis=1).float()
+        count+= torch.sum(x_pred[:,:s]>1,axis=1).float()    
+
+        #Validity         
+        temp_logits = self.pred_model(x_pred)
+        validity_loss= torch.zeros(1)                                       
+        temp_1= temp_logits[target_label==1,:]
+        temp_0= temp_logits[target_label==0,:]
+        validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_1[:,1]) - F.sigmoid(temp_1[:,0]), torch.tensor(-1), self.margin, reduction='mean')
+        validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_0[:,0]) - F.sigmoid(temp_0[:,1]), torch.tensor(-1), self.margin, reduction='mean')
+
+        for i in range(1,mc_samples):
+            x_pred = dm[i]       
+
+            recon_err += -torch.sum( torch.abs(x[:,s:-1] - x_pred[:,s:-1]), axis=1 )
             for key in self.normalise_weights.keys():
-                # recon_err+= -(1/mad_feature_weights[d.encoded_feature_names[int(key)]])*(normalise_weights[key][1] - normalise_weights[key][0])*torch.abs(x[:,key] - x_pred[:,key]) 
+                # recon_err+= -(1/mad_feature_weights[d.encoded_feature_names[int(key)]])*(normalise_weights[key][1] - normalise_weights[key][0])*torch.abs( (x[:,key] - x_pred[:,key]))
                 recon_err+= -(self.normalise_weights[key][1] - self.normalise_weights[key][0])*torch.abs(x[:,key] - x_pred[:,key]) 
 
             # Sum to 1 over the categorical indexes of a feature
-            for v in self.model.encoded_categorical_feature_indexes:
+            for v in self.cf_vae.encoded_categorical_feature_indexes:
                 temp = -torch.abs(  1.0-torch.sum( x_pred[:, v[0]:v[-1]+1], axis=1) )
                 recon_err += temp
 
-            count=0
             count+= torch.sum(x_pred[:,:s]<0,axis=1).float()
-            count+= torch.sum(x_pred[:,:s]>1,axis=1).float()    
+            count+= torch.sum(x_pred[:,:s]>1,axis=1).float()        
 
-            #Validity         
+            #Validity
             temp_logits = self.pred_model(x_pred)
-            validity_loss= torch.zeros(1)                                       
+    #         validity_loss += -F.cross_entropy(temp_logits, target_label)      
             temp_1= temp_logits[target_label==1,:]
             temp_0= temp_logits[target_label==0,:]
             validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_1[:,1]) - F.sigmoid(temp_1[:,0]), torch.tensor(-1), self.margin, reduction='mean')
             validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_0[:,0]) - F.sigmoid(temp_0[:,1]), torch.tensor(-1), self.margin, reduction='mean')
 
-            for i in range(1,mc_samples):
-                x_pred = dm[i]       
+        recon_err = recon_err / mc_samples
+        validity_loss = -1*self.validity_reg*validity_loss/mc_samples
 
-                recon_err += -torch.sum( torch.abs(x[:,s:-1] - x_pred[:,s:-1]), axis=1 )
-                for key in self.normalise_weights.keys():
-                    # recon_err+= -(1/mad_feature_weights[d.encoded_feature_names[int(key)]])*(normalise_weights[key][1] - normalise_weights[key][0])*torch.abs( (x[:,key] - x_pred[:,key]))
-                    recon_err+= -(self.normalise_weights[key][1] - self.normalise_weights[key][0])*torch.abs(x[:,key] - x_pred[:,key]) 
-
-                # Sum to 1 over the categorical indexes of a feature
-                for v in model.encoded_categorical_feature_indexes:
-                    temp = -torch.abs(  1.0-torch.sum( x_pred[:, v[0]:v[-1]+1], axis=1) )
-                    recon_err += temp
-
-                count+= torch.sum(x_pred[:,:s]<0,axis=1).float()
-                count+= torch.sum(x_pred[:,:s]>1,axis=1).float()        
-
-                #Validity
-                temp_logits = self.pred_model(x_pred)
-        #         validity_loss += -F.cross_entropy(temp_logits, target_label)      
-                temp_1= temp_logits[target_label==1,:]
-                temp_0= temp_logits[target_label==0,:]
-                validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_1[:,1]) - F.sigmoid(temp_1[:,0]), torch.tensor(-1), self.margin, reduction='mean')
-                validity_loss += F.hinge_embedding_loss( F.sigmoid(temp_0[:,0]) - F.sigmoid(temp_0[:,1]), torch.tensor(-1), self.margin, reduction='mean')
-
-            recon_err = recon_err / mc_samples
-            validity_loss = -1*self.validity_reg*validity_loss/mc_samples
-
-            print('Avg wrong cont dim: ', torch.mean(count)/mc_samples)
-            print('recon: ',-torch.mean(recon_err), ' KL: ', torch.mean(kl_divergence), ' Validity: ', -validity_loss)
-            return -torch.mean(recon_err - kl_divergence) - validity_loss
+        print('Avg wrong cont dim: ', torch.mean(count)/mc_samples)
+        print('recon: ',-torch.mean(recon_err), ' KL: ', torch.mean(kl_divergence), ' Validity: ', -validity_loss)
+        return -torch.mean(recon_err - kl_divergence) - validity_loss
 
     
-    def train( self ):
+    def train(self):
+        
+        ##TODO: Handling such dataset specific constraints in a more general way
+        # CF Generation for only low to high income data points
+        self.vae_train_dataset= self.vae_train_dataset[self.vae_train_dataset[:,-1]==0,:]
+        self.vae_val_dataset= self.vae_val_dataset[self.vae_val_dataset[:,-1]==0,:]
+
+        #Removing the outcome variable from the datasets
+        self.vae_train_feat= self.vae_train_dataset[:,:-1]
+        self.vae_val_feat= self.vae_val_dataset[:,:-1]
         
         for epoch in range(self.epochs):
             batch_num=0
             train_loss= 0.0
             train_size=0
             
-            train_dataset= torch.tensor(self.vae_train_dataset).float()
+            train_dataset= torch.tensor(self.vae_train_feat).float()
             train_dataset= torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
             for train_x in enumerate(train_dataset):
                 self.cf_vae_optimizer.zero_grad()
@@ -182,18 +197,21 @@ class DiceBaseGenCF:
                 loss= self.compute_loss( out, train_x, train_y )
                 loss.backward()
                 train_loss += loss.item()
-                optimizer.step()
+                self.cf_vae_optimizer.step()
                 
                 batch_num+=1
                 
             ret= loss/batch_num
             print('Train Avg Loss: ', ret, train_size )
             
-    ## I plan to keep the input arguments for this function same as the one defined for Diverse CF
-    ## Questions: Would query_instance a numpy_matrix
-    ## In what format do I need to return the predicted countefactuals
-    
-    def generate_countefactuals(self, query_instance, total_CFs, desired_class="opposite" ):
+            base_model_dir= '../dice_ml/utils/sample_trained_models/'
+            ##TODO: A general way to get the dataset name or a better to save the pre trained models and load them for the next time
+            dataset_name= 'adult'
+            path=base_model_dir+ dataset_name +'-margin-' + str(self.margin) + '-validity_reg-'+ str(self.validity_reg) + '-epoch-' + str(self.epochs) + '-' + 'base-gen' + '.pth'
+            torch.save(self.cf_vae.state_dict(), path)                   
+            
+    #The input arguments for this function same as the one defined for Diverse CF    
+    def generate_countefactuals(self, query_instance, total_CFs, desired_class="opposite",  ):
         
         # Converting query_instance into numpy array
         query_instance_org= query_instance
@@ -214,9 +232,10 @@ class DiceBaseGenCF:
             train_x= torch.tensor( train_x ).float()
             train_y = torch.argmax( self.pred_model(train_x), dim=1 )                
             train_size += train_x.shape[0]        
+            
             curr_gen_cf=[]
             curr_cf_pred=[]            
-            curr_test_pred=[]
+            curr_test_pred= train_y.numpy()
             
             for cf_count in range(total_CFs):                
                 recon_err, kl_err, x_true, x_pred, cf_label = model.compute_elbo( train_x, 1.0-train_y, pred_model )
@@ -234,5 +253,6 @@ class DiceBaseGenCF:
         result={}
         result['CF']= final_gen_cf[0]
         result['CF-Pred']= final_cf_pred[0]
-        result['test-pred']= torch.argmax(  )
+        result['test-pred']= final_test_pred[0]
+        print(type(result['CF'], result['CF-Pred'], result['test-pred']))
         return exp.CounterfactualExamples(self.data_interface, query_instance_org, test_pred, final_gen_cf[0], final_cf_pred[0])
