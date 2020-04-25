c6a224627a7861f53cf8aa1251711e76802ede19
Ramaravind K M <raam.arvind93@gmail.com>
2020-04-25 12:56:59 +0530

    minor update to data interfaces
    Signed-off-by: divyat09 <divyatmahajan@gmail.com>

--- dice_ml/data_interfaces/private_data_interface.py
+++ dice_ml/data_interfaces/private_data_interface.py
@@ -165,7 +165,7 @@ class PrivateData:
                 if colidx in encoded_cats_ixs and col.startswith(tuple(features_to_vary)):
                     ixs.append(colidx)
                 elif colidx not in encoded_cats_ixs and col in features_to_vary:
-                    ixs.appen(colidx)
+                    ixs.append(colidx)
             return ixs
 
     def from_dummies(self, data, prefix_sep='_'):
