807f55dad6487ded51f51a025029293f2a1db6f4
Ramaravind K M <raam.arvind93@gmail.com>
2020-05-02 15:02:53 +0530

    Fixing private data arguments and updating tests for py3.6 since dict is not ordered by default  (#23)
    * private data support for py<=3.6
    
    * private data tests for py<=3.6 and minor updates to fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from collections import OrderedDict
 
 import dice_ml
 from dice_ml.utils import helpers
@@ -17,15 +18,15 @@ def private_data_object():
     """
     Returns a private data object containing meta information about the adult income dataset
     """
-    return dice_ml.Data(features={'age':[17, 90],
-                        'workclass': ['Government', 'Other/Unknown', 'Private', 'Self-Employed'],
-                       'education': ['Assoc', 'Bachelors', 'Doctorate', 'HS-grad', 'Masters', 'Prof-school', 'School', 'Some-college'],
-                       'marital_status': ['Divorced', 'Married', 'Separated', 'Single', 'Widowed'],
-                       'occupation':['Blue-Collar', 'Other/Unknown', 'Professional', 'Sales', 'Service', 'White-Collar'],
-                       'race': ['Other', 'White'],
-                       'gender':['Female', 'Male'],
-                       'hours_per_week': [1, 99]},
-             outcome_name='income')
+    features_dict = OrderedDict([('age', [17, 90]),
+                            ('workclass', ['Government', 'Other/Unknown', 'Private', 'Self-Employed']),
+                            ('education', ['Assoc', 'Bachelors', 'Doctorate', 'HS-grad', 'Masters', 'Prof-school', 'School', 'Some-college']),
+                            ('marital_status', ['Divorced', 'Married', 'Separated', 'Single', 'Widowed']),
+                            ('occupation', ['Blue-Collar', 'Other/Unknown', 'Professional', 'Sales', 'Service', 'White-Collar']),
+                            ('race', ['Other', 'White']),
+                            ('gender', ['Female', 'Male']),
+                            ('hours_per_week', [1, 99])]) # providing an OrderedDict to make it work for Python<=3.6
+    return dice_ml.Data(features=features_dict, outcome_name='income')
 
 @pytest.fixture
 def sample_adultincome_query():
@@ -34,15 +35,3 @@ def sample_adultincome_query():
     """
     return {'age':22, 'workclass':'Private', 'education':'HS-grad', 'marital_status':'Single', 'occupation':'Service',
     'race': 'White', 'gender':'Female', 'hours_per_week': 45}
-
-@pytest.fixture
-def query_instance():
-    query_instance = {'age':22,
-          'workclass':'Private',
-          'education':'HS-grad',
-          'marital_status':'Single',
-          'occupation':'Service',
-          'race': 'White',
-          'gender':'Female',
-          'hours_per_week': 45}
-    return query_instance
