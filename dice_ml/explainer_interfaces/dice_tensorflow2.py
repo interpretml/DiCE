d76cd209bb0d4fea79ed8afd23a626f64262eab4
Soundarya Krishnan <37398880+soundarya98@users.noreply.github.com>
2021-02-18 09:22:19 +0530

    Added features to vary & permitted range in dice_genetic (#66)
    * Added features to vary & permitted range in dice_genetic. Cleaned up permitted range code
    
    Signed-off-by: Soundarya Krishnan <soundaryak4898@gmail.com>
    
    * Fixed tests
    
    Signed-off-by: Soundarya Krishnan <soundaryak4898@gmail.com>

--- dice_ml/explainer_interfaces/dice_tensorflow2.py
+++ dice_ml/explainer_interfaces/dice_tensorflow2.py
@@ -85,7 +85,7 @@ class DiceTensorFlow2(ExplainerBase):
 
         # check permitted range for continuous features
         if permitted_range is not None:
-            if not self.data_interface.check_features_range():
+            if not self.data_interface.check_features_range(permitted_range):
                 raise ValueError(
                     "permitted range of features should be within their original range")
             else:
