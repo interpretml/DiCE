cb5e936b176e49d6edc56ea621cafac2e8d1ca5c
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2021-02-27 21:56:35 -0800

    Sort importance values in CounterfactualExplanations (#93)
    Signed-off-by: gaugup <gaugup@microsoft.com>

--- tests/test_counterfactual_explanations.py
+++ tests/test_counterfactual_explanations.py
@@ -6,10 +6,21 @@ from dice_ml.counterfactual_explanations import CounterfactualExplanations
 class TestCounterfactualExplanations:
 
     def test_counterfactual_explanations_class(self):
-        counterfactual_explanations = CounterfactualExplanations(cf_examples_list=[])
+        counterfactual_explanations = CounterfactualExplanations(
+            cf_examples_list=[],
+            local_importance=None,
+            summary_importance={
+                "age": 0.985,
+                "workclass": 0.65,
+                "education": 0.915,
+                "occupation": 0.95,
+                "hours_per_week": 0.985,
+                "gender": 0.67,
+                "marital_status": 0.655,
+                "race": 0.41})
         assert counterfactual_explanations.cf_examples_list is not None
         assert len(counterfactual_explanations.cf_examples_list) == 0
-        assert counterfactual_explanations.summary_importance is None
+        assert counterfactual_explanations.summary_importance is not None
         assert counterfactual_explanations.local_importance is None
         assert counterfactual_explanations.metadata is not None
         assert counterfactual_explanations.metadata['version'] is not None
