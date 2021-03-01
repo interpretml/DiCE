b42304163f640b33727787bc92d16226d872e1f4
Amit Sharma <amit_sharma@live.com>
2021-03-01 13:26:36 +0530

    updated private interface and feasible-cf notebooks to work with dataframe input (#96)
    * updated private interface to work with refactor
    
    * fixed feasible cf and advanced notebooks
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>

--- dice_ml/explainer_interfaces/feasible_model_approx.py
+++ dice_ml/explainer_interfaces/feasible_model_approx.py
@@ -27,15 +27,17 @@ class FeasibleModelApprox(FeasibleBaseVAE, ExplainerBase):
         """
         :param data_interface: an interface class to data related params
         :param model_interface: an interface class to access trained ML model
-        """    
-        
+        """
+
         # initiating data related parameters
-        ExplainerBase.__init__(self, data_interface) 
-        
+        ExplainerBase.__init__(self, data_interface)
+
         #Black Box ML Model to be explained
-        self.pred_model= model_interface.model        
-        
-        #Hyperparam 
+        self.pred_model= model_interface.model
+
+        self.minx, self.maxx, self.encoded_categorical_feature_indexes, self.encoded_continuous_feature_indexes, self.cont_minx, self.cont_maxx, self.cont_precisions = self.data_interface.get_data_params_for_gradient_dice()
+        self.data_interface.one_hot_encoded_data = self.data_interface.one_hot_encode_data(self.data_interface.data_df)
+        #Hyperparam
         self.encoded_size=kwargs['encoded_size']
         self.learning_rate= kwargs['lr']
         self.batch_size= kwargs['batch_size']
@@ -45,34 +47,34 @@ class FeasibleModelApprox(FeasibleBaseVAE, ExplainerBase):
         self.wm1= kwargs['wm1']
         self.wm2= kwargs['wm2']
         self.wm3= kwargs['wm3']
-               
-        #Initializing parameters for the DiceModelApproxGenCF  
-        self.vae_train_dataset, self.vae_val_dataset, self.vae_test_dataset, self.normalise_weights, self.cf_vae, self.cf_vae_optimizer= get_base_gen_cf_initialization( self.data_interface, self.encoded_size, self.cont_minx, self.cont_maxx, self.margin, self.validity_reg, self.epochs, self.wm1, self.wm2, self.wm3, self.learning_rate ) 
-        
+
+        #Initializing parameters for the DiceModelApproxGenCF
+        self.vae_train_dataset, self.vae_val_dataset, self.vae_test_dataset, self.normalise_weights, self.cf_vae, self.cf_vae_optimizer= get_base_gen_cf_initialization( self.data_interface, self.encoded_size, self.cont_minx, self.cont_maxx, self.margin, self.validity_reg, self.epochs, self.wm1, self.wm2, self.wm3, self.learning_rate )
+
         #Data paths
-        self.base_model_dir= '../../dice_ml/utils/sample_trained_models/'
+        self.base_model_dir= '../../../dice_ml/utils/sample_trained_models/'
         self.save_path=self.base_model_dir+ self.data_interface.data_name +'-margin-' + str(self.margin) + '-validity_reg-'+ str(self.validity_reg) + '-epoch-' + str(self.epochs) + '-' + 'ae-gen' + '.pth'
-    
-    
+
+
     def train( self, constraint_type, constraint_variables, constraint_direction, constraint_reg, pre_trained=False  ):
-        
-        '''        
-        
-        :param pre_trained: Bool Variable to check whether pre trained model exists to avoid training again        
+
+        '''
+
+        :param pre_trained: Bool Variable to check whether pre trained model exists to avoid training again
         :param constraint_type: Binary Variable currently: (1) unary / (0) monotonic
         :param constraint_variables: List of List: [[Effect, Cause1, Cause2, .... ]]
         :param constraint_direction: -1: Negative, 1: Positive ( By default has to be one for monotonic constraints )
         :param constraint_reg: Tunable Hyperparamter
-        
+
         :return None
-        
+
         '''
-        
+
         if pre_trained:
             self.cf_vae.load_state_dict(torch.load(self.save_path))
             self.cf_vae.eval()
-            return 
-        
+            return
+
         ##TODO: Handling such dataset specific constraints in a more general way
         # CF Generation for only low to high income data points
         self.vae_train_dataset= self.vae_train_dataset[self.vae_train_dataset[:,-1]==0,:]
@@ -80,25 +82,25 @@ class FeasibleModelApprox(FeasibleBaseVAE, ExplainerBase):
 
         #Removing the outcome variable from the datasets
         self.vae_train_feat= self.vae_train_dataset[:,:-1]
-        self.vae_val_feat= self.vae_val_dataset[:,:-1]        
-        
+        self.vae_val_feat= self.vae_val_dataset[:,:-1]
+
         for epoch in range(self.epochs):
             batch_num=0
             train_loss= 0.0
             train_size=0
-            
+
             train_dataset= torch.tensor(self.vae_train_feat).float()
             train_dataset= torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
             for train_x in enumerate(train_dataset):
                 self.cf_vae_optimizer.zero_grad()
-                
+
                 train_x= train_x[1]
                 train_y= 1.0-torch.argmax( self.pred_model(train_x), dim=1 )
                 train_size+= train_x.shape[0]
-                
+
                 out= self.cf_vae(train_x, train_y)
                 loss= self.compute_loss( out, train_x, train_y )
-                
+
                 #Unary Case
                 if constraint_type:
                     for const in constraint_variables:
@@ -108,13 +110,13 @@ class FeasibleModelApprox(FeasibleBaseVAE, ExplainerBase):
                         dm = out['x_pred']
                         mc_samples = out['mc_samples']
                         x_pred = dm[0]
-                       
+
                         constraint_loss = F.hinge_embedding_loss( constraint_direction*(x_pred[:,const_idx] - train_x[:,const_idx]), torch.tensor(-1), 0)
 
                         for j in range(1, mc_samples):
-                            x_pred = dm[j]            
-                            constraint_loss+= F.hinge_embedding_loss( constraint_direction*(x_pred[:,const_idx] - train_x[:,const_idx]), torch.tensor(-1), 0)           
-                            
+                            x_pred = dm[j]
+                            constraint_loss+= F.hinge_embedding_loss( constraint_direction*(x_pred[:,const_idx] - train_x[:,const_idx]), torch.tensor(-1), 0)
+
                         constraint_loss= constraint_loss/mc_samples
                         constraint_loss= constraint_reg*constraint_loss
                         loss+= constraint_loss
@@ -122,16 +124,16 @@ class FeasibleModelApprox(FeasibleBaseVAE, ExplainerBase):
                 else:
                     #Train the regression model
                     raise NotImplementedError("This has not been implemented yet. If you'd like this to be implemented in the next version, please raise an issue at https://github.com/interpretml/DiCE/issues")
-                    
+
                 loss.backward()
                 train_loss += loss.item()
                 self.cf_vae_optimizer.step()
-                
+
                 batch_num+=1
-                
+
             ret= loss/batch_num
             print('Train Avg Loss: ', ret, train_size )
-            
+
             #Save the model after training every 10 epochs and at the last epoch
             if (epoch!=0 and epoch%10==0) or epoch==self.epochs-1:
                 torch.save(self.cf_vae.state_dict(), self.save_path)
--- dice_ml/utils/sample_architecture/vae_model.py
+++ dice_ml/utils/sample_architecture/vae_model.py
@@ -7,20 +7,20 @@ from torchvision.utils import save_image
 from torch.autograd import Variable
 
 class CF_VAE(nn.Module):
-    
+
     def __init__(self, d, encoded_size):
 
         super(CF_VAE, self).__init__()
-        
+
         self.encoded_size = encoded_size
-        self.data_size = len(d.encoded_feature_names)
-        self.minx, self.maxx, self.encoded_categorical_feature_indexes = d.get_data_params()
+        self.data_size = len(d.ohe_encoded_feature_names)
+        self.minx, self.maxx, self.encoded_categorical_feature_indexes, self.encoded_continuous_feature_indexes, self.cont_minx, self.cont_maxx, self.cont_precisions = d.get_data_params_for_gradient_dice()
 
         flattened_indexes = [item for sublist in self.encoded_categorical_feature_indexes for item in sublist]
-        self.encoded_continuous_feature_indexes = [ix for ix in range(len(self.minx[0])) if ix not in flattened_indexes]        
+        self.encoded_continuous_feature_indexes = [ix for ix in range(len(self.minx[0])) if ix not in flattened_indexes]
         self.encoded_start_cat = len(self.encoded_continuous_feature_indexes)
 
-        # Plus 1 to the input encoding size and data size to incorporate the target class label        
+        # Plus 1 to the input encoding size and data size to incorporate the target class label
         self.encoder_mean = nn.Sequential(
             nn.Linear( self.data_size+1, 20 ),
             nn.BatchNorm1d(20),
@@ -83,7 +83,7 @@ class CF_VAE(nn.Module):
             nn.Linear( 20, self.data_size),
             nn.Sigmoid()
             )
-        
+
     def encoder(self, x):
         mean = self.encoder_mean(x)
         logvar = 0.5+ self.encoder_var(x)
@@ -92,17 +92,17 @@ class CF_VAE(nn.Module):
     def decoder(self, z):
         mean = self.decoder_mean(z)
         return mean
-    
+
     def sample_latent_code(self, mean, logvar):
         eps = torch.randn_like(logvar)
         return mean + torch.sqrt(logvar)*eps
 
     def normal_likelihood(self, x, mean, logvar, raxis=1):
         return torch.sum( -.5 * ((x - mean)*(1./logvar)*(x-mean) + torch.log(logvar) ), axis=1)
-                          
+
     def forward(self, x, c):
         c=c.view( c.shape[0], 1 )
-        c=torch.tensor(c).float()        
+        c=torch.tensor(c).float()
         res={}
         mc_samples=50
         em, ev= self.encoder( torch.cat((x,c),1) )
@@ -116,31 +116,31 @@ class CF_VAE(nn.Module):
             x_pred= self.decoder( torch.cat((z,c),1) )
             res['z'].append(z)
             res['x_pred'].append(x_pred)
-    
+
         return res
 
     def compute_elbo(self, x, c, pred_model):
         c=torch.tensor(c).float()
-        c=c.view( c.shape[0], 1 )    
+        c=c.view( c.shape[0], 1 )
         em, ev = self.encoder( torch.cat((x,c),1) )
-        kl_divergence = 0.5*torch.mean( em**2 +ev - torch.log(ev) - 1, axis=1 ) 
+        kl_divergence = 0.5*torch.mean( em**2 +ev - torch.log(ev) - 1, axis=1 )
 
         z = self.sample_latent_code(em, ev)
         dm= self.decoder( torch.cat((z,c),1) )
         log_px_z = torch.tensor(0.0)
-        
+
         x_pred= dm
         return torch.mean(log_px_z), torch.mean(kl_divergence), x, x_pred, torch.argmax( pred_model(x_pred), dim=1 )
 
 class AutoEncoder(nn.Module):
-    
+
     def __init__(self, d, encoded_size):
 
         super(AutoEncoder, self).__init__()
-        
+
         self.encoded_size = encoded_size
         self.data_size = len(d.encoded_feature_names)
-        self.encoded_categorical_feature_indexes = d.get_data_params()[2]     
+        self.encoded_categorical_feature_indexes = d.get_data_params()[2]
 
         self.encoded_continuous_feature_indexes=[]
         for i in range(self.data_size):
@@ -149,10 +149,10 @@ class AutoEncoder(nn.Module):
                 if i in v:
                     valid=0
             if valid:
-                self.encoded_continuous_feature_indexes.append(i)            
+                self.encoded_continuous_feature_indexes.append(i)
 
         self.encoded_start_cat = len(self.encoded_continuous_feature_indexes)
-        
+
         self.encoder_mean = nn.Sequential(
             nn.Linear( self.data_size, 20 ),
             nn.BatchNorm1d(20),
@@ -214,7 +214,7 @@ class AutoEncoder(nn.Module):
             nn.Linear( 20, self.data_size),
             nn.Sigmoid()
             )
-        
+
     def encoder(self, x):
         mean = self.encoder_mean(x)
         logvar = 0.05+ self.encoder_var(x)
@@ -223,15 +223,15 @@ class AutoEncoder(nn.Module):
     def decoder(self, z):
         mean = self.decoder_mean(z)
         return mean
-    
+
     def sample_latent_code(self, mean, logvar):
         eps = torch.randn_like(logvar)
         return mean + torch.sqrt(logvar)*eps
 
     def normal_likelihood(self, x, mean, logvar, raxis=1):
         return torch.sum( -.5 * ((x - mean)*(1./logvar)*(x-mean) + torch.log(logvar) ), axis=1)
-                          
-    def forward(self, x):        
+
+    def forward(self, x):
         res={}
         mc_samples=50
         em, ev= self.encoder(x)
@@ -245,6 +245,6 @@ class AutoEncoder(nn.Module):
             x_pred= self.decoder(z)
             res['z'].append(z)
             res['x_pred'].append(x_pred)
-    
+
         return res
 
