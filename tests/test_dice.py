fddc9719e96c4f553d8651fb55db480456e45d2e
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2021-03-08 01:16:41 -0800

    Move string literals to module constants (#100)
    Signed-off-by: gaugup <gaugup@microsoft.com>

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -6,12 +6,12 @@ from dice_ml.utils.exception import UserConfigValidationException
 
 
 class TestBaseExplainerLoader:
-    def _get_exp(self, backend):
+    def _get_exp(self, backend, method="random"):
         dataset = helpers.load_adult_income_dataset()
         d = dice_ml.Data(dataframe=dataset, continuous_features=['age', 'hours_per_week'], outcome_name='income')
         ML_modelpath = helpers.get_adult_income_modelpath(backend=backend)
         m = dice_ml.Model(model_path= ML_modelpath, backend=backend)
-        exp = dice_ml.Dice(d, m)
+        exp = dice_ml.Dice(d, m, method=method)
         return exp
 
     def test_tf(self):
@@ -19,12 +19,23 @@ class TestBaseExplainerLoader:
         backend = 'TF'+tf.__version__[0]
         exp = self._get_exp(backend)
         assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
+        assert isinstance(exp, dice_ml.explainer_interfaces.dice_tensorflow2.DiceTensorFlow2) or \
+            isinstance(exp, dice_ml.explainer_interfaces.dice_tensorflow1.DiceTensorFlow1)
 
     def test_pyt(self):
         pyt = pytest.importorskip("torch")
         backend = 'PYT'
         exp = self._get_exp(backend)
         assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
+        assert isinstance(exp, dice_ml.explainer_interfaces.dice_pytorch.DicePyTorch)
+
+    @pytest.mark.parametrize('method', ['random'])
+    def test_sklearn(self, method):
+        sklearn = pytest.importorskip("sklearn")
+        backend = 'sklearn'
+        exp = self._get_exp(backend, method=method)
+        assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
+        assert isinstance(exp, dice_ml.explainer_interfaces.dice_random.DiceRandom)
 
     def test_minimum_query_instances(self):
         sklearn_model = pytest.importorskip('sklearn')
