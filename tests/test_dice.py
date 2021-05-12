c961f7c479f8f9fb4ea64bbfc71c34b4014974fc
gaugup <gaugup@microsoft.com>
2021-05-12 11:40:43 -0700

    Move data loading in a fixture
    Signed-off-by: gaugup <gaugup@microsoft.com>

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -6,47 +6,44 @@ from dice_ml.utils.exception import UserConfigValidationException
 
 
 class TestBaseExplainerLoader:
-    def _get_exp(self, backend, method="random"):
-        dataset = helpers.load_adult_income_dataset()
-        d = dice_ml.Data(dataframe=dataset, continuous_features=['age', 'hours_per_week'], outcome_name='income')
+    def _get_exp(self, backend, method="random", diceml_data=None):
         ML_modelpath = helpers.get_adult_income_modelpath(backend=backend)
         m = dice_ml.Model(model_path=ML_modelpath, backend=backend)
-        exp = dice_ml.Dice(d, m, method=method)
+        exp = dice_ml.Dice(diceml_data, m, method=method)
         return exp
 
-    def test_tf(self):
+    def test_tf(self, public_data_object):
         tf = pytest.importorskip("tensorflow")
         backend = 'TF'+tf.__version__[0]
-        exp = self._get_exp(backend)
+        exp = self._get_exp(backend, diceml_data=public_data_object)
         assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
         assert isinstance(exp, dice_ml.explainer_interfaces.dice_tensorflow2.DiceTensorFlow2) or \
             isinstance(exp, dice_ml.explainer_interfaces.dice_tensorflow1.DiceTensorFlow1)
 
-    def test_pyt(self):
+    def test_pyt(self, public_data_object):
         pytest.importorskip("torch")
         backend = 'PYT'
-        exp = self._get_exp(backend)
+        exp = self._get_exp(backend, diceml_data=public_data_object)
         assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
         assert isinstance(exp, dice_ml.explainer_interfaces.dice_pytorch.DicePyTorch)
 
     @pytest.mark.parametrize('method', ['random'])
-    def test_sklearn(self, method):
+    def test_sklearn(self, method, public_data_object):
         pytest.importorskip("sklearn")
         backend = 'sklearn'
-        exp = self._get_exp(backend, method=method)
+        exp = self._get_exp(backend, method=method, diceml_data=public_data_object)
         assert issubclass(type(exp), dice_ml.explainer_interfaces.explainer_base.ExplainerBase)
         assert isinstance(exp, dice_ml.explainer_interfaces.dice_random.DiceRandom)
 
-    def test_minimum_query_instances(self):
+    def test_minimum_query_instances(self, public_data_object, sample_adultincome_query):
         pytest.importorskip('sklearn')
         backend = 'sklearn'
-        exp = self._get_exp(backend)
+        exp = self._get_exp(backend, diceml_data=public_data_object)
         with pytest.raises(UserConfigValidationException):
-            query_instances = helpers.load_adult_income_dataset().drop("income", axis=1)[0:1]
-            exp.global_feature_importance(query_instances)
+            exp.global_feature_importance(sample_adultincome_query)
 
-    def test_unsupported_sampling_strategy(self):
+    def test_unsupported_sampling_strategy(self, public_data_object):
         pytest.importorskip('sklearn')
         backend = 'sklearn'
         with pytest.raises(UserConfigValidationException):
-            self._get_exp(backend, method="unsupported")
+            self._get_exp(backend, method="unsupported", diceml_data=public_data_object)
