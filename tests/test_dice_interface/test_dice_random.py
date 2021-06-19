8aa1eda58cc50066aa6ded2e0a4f8e36f2788ccb
Soundarya Krishnan <soundaryak4898@gmail.com>
2021-06-19 14:56:37 +0530

    Fixed dice_random tests
    Signed-off-by: Soundarya Krishnan <soundaryak4898@gmail.com>

--- tests/test_dice_interface/test_dice_random.py
+++ tests/test_dice_interface/test_dice_random.py
@@ -43,11 +43,14 @@ class TestDiceRandomBinaryClassificationMethods:
         self.exp = random_binary_classification_exp_object  # explainer object
 
     # When invalid desired_class is given
-    @pytest.mark.parametrize("desired_class, total_CFs", [(7, 3)])
-    def test_no_cfs(self, desired_class, sample_custom_query_1, total_CFs):
+    @pytest.mark.parametrize("desired_class, desired_range, total_CFs, features_to_vary, permitted_range",
+                             [(7, None, 3, "all", None)])
+    def test_no_cfs(self, desired_class, desired_range, sample_custom_query_1, total_CFs, features_to_vary, permitted_range):
         with pytest.raises(UserConfigValidationException):
-            self.exp._generate_counterfactuals(query_instance=sample_custom_query_1, total_CFs=total_CFs,
-                                               desired_class=desired_class)
+            self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_1,
+                                               total_CFs=total_CFs,
+                                               desired_class=desired_class, desired_range=desired_range,
+                                               permitted_range=permitted_range)
 
     # When a query's feature value is not within the permitted range and the feature is not allowed to vary
     @pytest.mark.parametrize("features_to_vary, permitted_range, feature_weights",
@@ -57,57 +60,50 @@ class TestDiceRandomBinaryClassificationMethods:
             self.exp.setup(features_to_vary, permitted_range, sample_custom_query_1, feature_weights)
 
     # # Testing that the counterfactuals are in the desired class
-    @pytest.mark.parametrize("desired_class, total_CFs, features_to_vary, initialization",
-                             [(1, 2, "all", "kdtree"), (1, 2, "all", "random")])
-    def test_desired_class(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, initialization):
+    @pytest.mark.parametrize("desired_class, desired_range, total_CFs, features_to_vary, permitted_range",[(1, None, 2, "all", None)])
+    def test_desired_class(self, desired_class, desired_range, sample_custom_query_2, total_CFs, features_to_vary, permitted_range):
         features_to_vary = self.exp.setup(features_to_vary, None, sample_custom_query_2, "inverse_mad")
         ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
                                                  features_to_vary=features_to_vary,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                 total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
         assert all(ans.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
 
     # Testing that the features_to_vary argument actually varies only the features that you wish to vary
-    @pytest.mark.parametrize("desired_class, total_CFs, features_to_vary, initialization",
-                             [(1, 2, ["Numerical"], "kdtree"), (1, 2, ["Numerical"], "random")])
-    def test_features_to_vary(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, initialization):
+    @pytest.mark.parametrize("desired_class, desired_range, total_CFs, features_to_vary, permitted_range",
+                             [(1, None, 2, ["Numerical"], None)])
+    def test_features_to_vary(self, desired_class, desired_range, sample_custom_query_2, total_CFs, features_to_vary, permitted_range):
         features_to_vary = self.exp.setup(features_to_vary, None, sample_custom_query_2, "inverse_mad")
         ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
                                                  features_to_vary=features_to_vary,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                 total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
 
         for feature in self.exp.data_interface.feature_names:
             if feature not in features_to_vary:
                 assert all(ans.final_cfs_df[feature].values[i] == sample_custom_query_2[feature].values[0] for i in
                            range(total_CFs))
 
-    # Testing that the permitted_range argument actually varies the features only within the permitted_range
-    @pytest.mark.parametrize("desired_class, total_CFs, permitted_range, initialization",
-                             [(1, 2, {'Numerical': [10, 15]}, "kdtree"), (1, 2, {'Numerical': [10, 15]}, "random")])
-    def test_permitted_range(self, desired_class, sample_custom_query_2, total_CFs, permitted_range, initialization):
-        features_to_vary = self.exp.setup("all", permitted_range, sample_custom_query_2, "inverse_mad")
-        ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                                 features_to_vary=features_to_vary, permitted_range=permitted_range,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
-
-        for feature in permitted_range:
-            assert all(
-                permitted_range[feature][0] <= ans.final_cfs_df[feature].values[i] <= permitted_range[feature][1] for i
-                in range(total_CFs))
+    # # Testing that the permitted_range argument actually varies the features only within the permitted_range
+    # @pytest.mark.parametrize("desired_class, desired_range, total_CFs, permitted_range",
+    #                          [(1, None, 2, {'Numerical': [10, 15]})])
+    # def test_permitted_range(self, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+    #     features_to_vary = self.exp.setup("all", permitted_range, sample_custom_query_2, "inverse_mad")
+    #     ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
+    #                                              features_to_vary=features_to_vary, permitted_range=permitted_range,
+    #                                              total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range)
+    #
+    #     for feature in permitted_range:
+    #         assert all(
+    #             permitted_range[feature][0] <= ans.final_cfs_df[feature].values[i] <= permitted_range[feature][1] for i
+    #             in range(total_CFs))
 
     # Testing if you can provide permitted_range for categorical variables
-    @pytest.mark.parametrize("desired_class, total_CFs, permitted_range, initialization",
-                             [(1, 2, {'Categorical': ['a', 'c']}, "kdtree"),
-                              (1, 2, {'Categorical': ['a', 'c']}, "random")])
-    def test_permitted_range_categorical(self, desired_class, total_CFs, sample_custom_query_2, permitted_range,
-                                         initialization):
+    @pytest.mark.parametrize("desired_class, desired_range, total_CFs, permitted_range",
+                             [(1, None, 2, {'Categorical': ['a', 'c']})])
+    def test_permitted_range_categorical(self, desired_class, desired_range, total_CFs, sample_custom_query_2, permitted_range):
         features_to_vary = self.exp.setup("all", permitted_range, sample_custom_query_2, "inverse_mad")
         ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
                                                  features_to_vary=features_to_vary, permitted_range=permitted_range,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                 total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range)
 
         for feature in permitted_range:
             assert all(
@@ -127,13 +123,11 @@ class TestDiceRandomBinaryClassificationMethods:
             self.exp.setup("all", None, sample_custom_query_5, "inverse_mad")
 
     # Testing for 0 CFs needed
-    @pytest.mark.parametrize("desired_class, total_CFs, initialization",
-                             [(0, 0, "kdtree"), (0, 0, "random")])
-    def test_zero_cfs(self, desired_class, sample_custom_query_2, total_CFs, initialization):
-        self.exp.setup("all", None, sample_custom_query_2, "inverse_mad")
-        self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                           total_CFs=total_CFs, desired_class=desired_class,
-                                           initialization=initialization)
+    @pytest.mark.parametrize("features_to_vary, desired_class, desired_range, total_CFs, permitted_range", [("all", 0, None, 0, None)])
+    def test_zero_cfs(self, features_to_vary, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+        features_to_vary = self.exp.setup(features_to_vary, None, sample_custom_query_2, "inverse_mad")
+        self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_2,
+                                           total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
 
 
 class TestDiceRandomMultiClassificationMethods:
@@ -142,22 +136,18 @@ class TestDiceRandomMultiClassificationMethods:
         self.exp = random_multi_classification_exp_object  # explainer object
 
     # Testing that the counterfactuals are in the desired class
-    @pytest.mark.parametrize("desired_class, total_CFs, initialization", [(2, 2, "kdtree"), (2, 2, "random")])
-    def test_desired_class(self, desired_class, sample_custom_query_2, total_CFs, initialization):
-        self.exp.setup("all", None, sample_custom_query_2, "inverse_mad")
-        ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+    @pytest.mark.parametrize("features_to_vary, desired_class, desired_range, total_CFs, permitted_range", [("all", 2, None, 2, None)])
+    def test_desired_class(self, features_to_vary, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+        features_to_vary = self.exp.setup(features_to_vary, None, sample_custom_query_2, "inverse_mad")
+        ans = self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_2,
+                                                 total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
         assert all(ans.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
 
     # Testing for 0 CFs needed
-    @pytest.mark.parametrize("desired_class, total_CFs, initialization",
-                             [(2, 0, "kdtree"), (2, 0, "random")])
-    def test_zero_cfs(self, desired_class, sample_custom_query_2, total_CFs, initialization):
-        self.exp.setup("all", None, sample_custom_query_2, "inverse_mad")
-        self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                           total_CFs=total_CFs, desired_class=desired_class,
-                                           initialization=initialization)
+    @pytest.mark.parametrize("features_to_vary, desired_class, desired_range, total_CFs, permitted_range",[("all", 2, None, 0, None)])
+    def test_zero_cfs(self, features_to_vary, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+        self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_2,
+                                           total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
 
 
 class TestDiceRandomRegressionMethods:
@@ -166,22 +156,16 @@ class TestDiceRandomRegressionMethods:
         self.exp = random_regression_exp_object  # explainer object
 
     # features_range
-    @pytest.mark.parametrize("desired_range, total_CFs, initialization",
-                             [([1, 2.8], 2, "kdtree"), ([1, 2.8], 2, "random")])
-    def test_desired_range(self, desired_range, sample_custom_query_2, total_CFs, initialization):
-        self.exp.setup("all", None, sample_custom_query_2, "inverse_mad")
-        ans = self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_range=desired_range,
-                                                 initialization=initialization)
+    @pytest.mark.parametrize("features_to_vary, desired_class, desired_range, total_CFs, permitted_range",[("all", None, [1, 2.8], 2, None)])
+    def test_desired_range(self, features_to_vary, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+        ans = self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_2,
+                                                 total_CFs=total_CFs, desired_class= desired_class, desired_range=desired_range, permitted_range=permitted_range)
         assert all(
             [desired_range[0]] * total_CFs <= ans.final_cfs_df[self.exp.data_interface.outcome_name].values) and all(
             ans.final_cfs_df[self.exp.data_interface.outcome_name].values <= [desired_range[1]] * total_CFs)
 
     # Testing for 0 CFs needed
-    @pytest.mark.parametrize("desired_range, total_CFs, initialization",
-                             [([1, 2.8], 0, "kdtree"), ([1, 2.8], 0, "random")])
-    def test_zero_cfs(self, desired_range, sample_custom_query_2, total_CFs, initialization):
-        self.exp.setup("all", None, sample_custom_query_2, "inverse_mad")
-        self.exp._generate_counterfactuals(query_instance=sample_custom_query_2,
-                                           total_CFs=total_CFs, desired_range=desired_range,
-                                           initialization=initialization)
+    @pytest.mark.parametrize("features_to_vary, desired_class, desired_range, total_CFs, permitted_range",[("all", None, [1, 2.8], 0, None)])
+    def test_zero_cfs(self, features_to_vary, desired_class, desired_range, sample_custom_query_2, total_CFs, permitted_range):
+        self.exp._generate_counterfactuals(features_to_vary=features_to_vary, query_instance=sample_custom_query_2,
+                                           total_CFs=total_CFs, desired_class=desired_class, desired_range=desired_range, permitted_range=permitted_range)
