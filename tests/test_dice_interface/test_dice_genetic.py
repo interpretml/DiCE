8d707b91a94a05053431638154586cbb3ea2e142
Soundarya Krishnan <soundaryak4898@gmail.com>
2021-07-28 19:26:18 +0530

    Reformatted file to fix lint
    Signed-off-by: Soundarya Krishnan <soundaryak4898@gmail.com>

--- tests/test_dice_interface/test_dice_genetic.py
+++ tests/test_dice_interface/test_dice_genetic.py
@@ -47,7 +47,7 @@ class TestDiceGeneticBinaryClassificationMethods:
     def test_no_cfs(self, desired_class, sample_custom_query_1, total_CFs):
         with pytest.raises(UserConfigValidationException):
             self.exp.generate_counterfactuals(query_instances=sample_custom_query_1, total_CFs=total_CFs,
-                                               desired_class=desired_class)
+                                              desired_class=desired_class)
 
     # When a query's feature value is not within the permitted range and the feature is not allowed to vary
     @pytest.mark.parametrize("features_to_vary, permitted_range, feature_weights",
@@ -61,57 +61,64 @@ class TestDiceGeneticBinaryClassificationMethods:
                              [(1, 2, "all", "kdtree"), (1, 2, "all", "random")])
     def test_desired_class(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 features_to_vary=features_to_vary,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                features_to_vary=features_to_vary,
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization)
         for cfs_example in ans.cf_examples_list:
-            assert all(cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
+            assert all(
+                cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
 
     # Testing that the features_to_vary argument actually varies only the features that you wish to vary
     @pytest.mark.parametrize("desired_class, total_CFs, features_to_vary, initialization",
                              [(1, 2, ["Numerical"], "kdtree"), (1, 2, ["Numerical"], "random")])
     def test_features_to_vary(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 features_to_vary=features_to_vary,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                features_to_vary=features_to_vary,
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization)
 
         for cfs_example in ans.cf_examples_list:
             for feature in self.exp.data_interface.feature_names:
                 if feature not in features_to_vary:
-                    assert all(cfs_example.final_cfs_df[feature].values[i] == sample_custom_query_2[feature].values[0] for i in
-                           range(total_CFs))
+                    assert all(
+                        cfs_example.final_cfs_df[feature].values[i] == sample_custom_query_2[feature].values[0] for i in
+                        range(total_CFs))
 
     # Testing that the permitted_range argument actually varies the features only within the permitted_range
     @pytest.mark.parametrize("desired_class, total_CFs, features_to_vary, permitted_range, initialization",
-                             [(1, 2, "all", {'Numerical': [10, 15]}, "kdtree"), (1, 2, "all", {'Numerical': [10, 15]}, "random")])
-    def test_permitted_range(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, permitted_range, initialization):
+                             [(1, 2, "all", {'Numerical': [10, 15]}, "kdtree"),
+                              (1, 2, "all", {'Numerical': [10, 15]}, "random")])
+    def test_permitted_range(self, desired_class, sample_custom_query_2, total_CFs, features_to_vary, permitted_range,
+                             initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 features_to_vary=features_to_vary, permitted_range=permitted_range,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                features_to_vary=features_to_vary, permitted_range=permitted_range,
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization)
 
         for cfs_example in ans.cf_examples_list:
             for feature in permitted_range:
                 assert all(
-                    permitted_range[feature][0] <= cfs_example.final_cfs_df[feature].values[i] <= permitted_range[feature][1] for i
+                    permitted_range[feature][0] <= cfs_example.final_cfs_df[feature].values[i] <=
+                    permitted_range[feature][1] for i
                     in range(total_CFs))
 
     # Testing if you can provide permitted_range for categorical variables
     @pytest.mark.parametrize("desired_class, total_CFs, features_to_vary, permitted_range, initialization",
                              [(1, 2, "all", {'Categorical': ['a', 'c']}, "kdtree"),
                               (1, 2, "all", {'Categorical': ['a', 'c']}, "random")])
-    def test_permitted_range_categorical(self, desired_class, total_CFs, features_to_vary, sample_custom_query_2, permitted_range,
+    def test_permitted_range_categorical(self, desired_class, total_CFs, features_to_vary, sample_custom_query_2,
+                                         permitted_range,
                                          initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 features_to_vary=features_to_vary, permitted_range=permitted_range,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization)
+                                                features_to_vary=features_to_vary, permitted_range=permitted_range,
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization)
 
         for cfs_example in ans.cf_examples_list:
             for feature in permitted_range:
                 assert all(
-                    permitted_range[feature][0] <= cfs_example.final_cfs_df[feature].values[i] <= permitted_range[feature][1] for i
+                    permitted_range[feature][0] <= cfs_example.final_cfs_df[feature].values[i] <=
+                    permitted_range[feature][1] for i
                     in range(total_CFs))
 
     # Testing if an error is thrown when the query instance has an unknown categorical variable
@@ -131,8 +138,8 @@ class TestDiceGeneticBinaryClassificationMethods:
                              [(0, 7, "kdtree", 0), (0, 7, "random", 0)])
     def test_maxiter(self, desired_class, sample_custom_query_2, total_CFs, initialization, maxiterations):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization, maxiterations=maxiterations)
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization, maxiterations=maxiterations)
         for cfs_example in ans.cf_examples_list:
             for i in cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values:
                 assert i == desired_class
@@ -157,18 +164,19 @@ class TestDiceGeneticMultiClassificationMethods:
     @pytest.mark.parametrize("desired_class, total_CFs, initialization", [(2, 2, "kdtree"), (2, 2, "random")])
     def test_desired_class(self, desired_class, sample_custom_query_2, total_CFs, initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
+                                                total_CFs=total_CFs, desired_class=desired_class,
                                                 initialization=initialization)
         for cfs_example in ans.cf_examples_list:
-            assert all(cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
+            assert all(
+                cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values == [desired_class] * total_CFs)
 
     # Testing if only valid cfs are found after maxiterations
     @pytest.mark.parametrize("desired_class, total_CFs, initialization, maxiterations",
                              [(2, 7, "kdtree", 0), (2, 7, "random", 0)])
     def test_maxiter(self, desired_class, sample_custom_query_2, total_CFs, initialization, maxiterations):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_class=desired_class,
-                                                 initialization=initialization, maxiterations=maxiterations)
+                                                total_CFs=total_CFs, desired_class=desired_class,
+                                                initialization=initialization, maxiterations=maxiterations)
         for cfs_example in ans.cf_examples_list:
             for i in cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values:
                 assert i == desired_class
@@ -194,11 +202,12 @@ class TestDiceGeneticRegressionMethods:
                              [([1, 2.8], 2, "kdtree"), ([1, 2.8], 2, "random")])
     def test_desired_range(self, desired_range, sample_custom_query_2, total_CFs, initialization):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_range=desired_range,
-                                                 initialization=initialization)
+                                                total_CFs=total_CFs, desired_range=desired_range,
+                                                initialization=initialization)
         for cfs_example in ans.cf_examples_list:
             assert all(
-                [desired_range[0]] * total_CFs <= cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values) and all(
+                [desired_range[0]] * total_CFs <= cfs_example.final_cfs_df[
+                    self.exp.data_interface.outcome_name].values) and all(
                 cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values <= [desired_range[1]] * total_CFs)
 
     # Testing if only valid cfs are found after maxiterations
@@ -206,8 +215,8 @@ class TestDiceGeneticRegressionMethods:
                              [([1, 2.8], 7, "kdtree", 0), ([1, 2.8], 7, "random", 0)])
     def test_maxiter(self, desired_range, sample_custom_query_2, total_CFs, initialization, maxiterations):
         ans = self.exp.generate_counterfactuals(query_instances=sample_custom_query_2,
-                                                 total_CFs=total_CFs, desired_range=desired_range,
-                                                 initialization=initialization, maxiterations=maxiterations)
+                                                total_CFs=total_CFs, desired_range=desired_range,
+                                                initialization=initialization, maxiterations=maxiterations)
         for cfs_example in ans.cf_examples_list:
             for i in cfs_example.final_cfs_df[self.exp.data_interface.outcome_name].values:
                 assert desired_range[0] <= i <= desired_range[1]
