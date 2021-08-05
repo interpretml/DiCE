44705b87de22bb6ce20f0b10ccb1d5b5586f0ff6
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2021-08-04 23:06:07 -1000

    Fix feature importance calculation (#199)
    * Fix feature importance computation
    
    Signed-off-by: gaugup <gaugup@microsoft.com>
    
    * handle when query_instances in None
    
    Signed-off-by: gaugup <gaugup@microsoft.com>
    
    * Add tests
    
    Signed-off-by: gaugup <gaugup@microsoft.com>
    
    * fix lint
    
    Signed-off-by: gaugup <gaugup@microsoft.com>
    
    * fixed lint error: too many empty lines
    
    Co-authored-by: Amit Sharma <amit_sharma@live.com>

--- dice_ml/explainer_interfaces/explainer_base.py
+++ dice_ml/explainer_interfaces/explainer_base.py
@@ -244,7 +244,7 @@ class ExplainerBase(ABC):
                   the list of counterfactuals per input, local feature importances per
                   input, and the global feature importance summarized over all inputs.
         """
-        if len(query_instances) < 10:
+        if query_instances is not None and len(query_instances) < 10:
             raise UserConfigValidationException("The number of query instances should be greater than or equal to 10")
         if cf_examples_list is not None:
             if any([len(cf_examples.final_cfs_df) < 10 for cf_examples in cf_examples_list]):
@@ -318,6 +318,7 @@ class ExplainerBase(ABC):
                 for col in allcols:
                     local_importances[i][col] = 0
 
+        overall_num_cfs = 0
         # Summarizing the found counterfactuals
         for i in range(len(cf_examples_list)):
             cf_examples = cf_examples_list[i]
@@ -331,7 +332,9 @@ class ExplainerBase(ABC):
             if df is None:
                 continue
 
+            per_query_point_cfs = 0
             for index, row in df.iterrows():
+                per_query_point_cfs += 1
                 for col in self.data_interface.continuous_feature_names:
                     if not np.isclose(org_instance[col].iat[0], row[col]):
                         if summary_importance is not None:
@@ -347,10 +350,16 @@ class ExplainerBase(ABC):
 
             if local_importances is not None:
                 for col in allcols:
-                    local_importances[i][col] /= (cf_examples_list[0].final_cfs_df.shape[0])
+                    if per_query_point_cfs > 0:
+                        local_importances[i][col] /= per_query_point_cfs
+
+            overall_num_cfs += per_query_point_cfs
+
         if summary_importance is not None:
             for col in allcols:
-                summary_importance[col] /= (cf_examples_list[0].final_cfs_df.shape[0]*len(cf_examples_list))
+                if overall_num_cfs > 0:
+                    summary_importance[col] /= overall_num_cfs
+
         return CounterfactualExplanations(
             cf_examples_list,
             local_importance=local_importances,
