8c92630d58866f1b4414d41aef3200f3d9851f75
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2022-01-25 00:01:45 -0800

    Add additional parameters to generate_counterfactuals() function (#251)
    * Add additional parameters to generate_counterfactuals() function
    
    Signed-off-by: gaugup <gaugup@microsoft.com>
    
    * added the docs for methods
    
    * Fix flake8 error
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    Co-authored-by: Amit Sharma <amit_sharma@live.com>

--- tests/test_dice_interface/test_explainer_base.py
+++ tests/test_dice_interface/test_explainer_base.py
@@ -365,6 +365,9 @@ class TestExplainerBaseBinaryClassification:
         ans = exp.generate_counterfactuals(query_instances=sample_custom_query_2,
                                            features_to_vary='all',
                                            total_CFs=2, desired_class=desired_class,
+                                           proximity_weight=0.2, sparsity_weight=0.2,
+                                           diversity_weight=5.0,
+                                           categorical_penalty=0.1,
                                            permitted_range=None)
         if method != 'kdtree':
             assert all(ans.cf_examples_list[0].final_cfs_df[exp.data_interface.outcome_name].values == [desired_class] * 2)
