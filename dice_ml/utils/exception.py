3129a8ea0c74e2acc5c00e299e124085f2acac65
Gaurav Gupta <gaugup@microsoft.com>
2023-06-26 21:43:43 -0700

    Remove deprecated class `UserConfigValidationException`
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- dice_ml/utils/exception.py
+++ dice_ml/utils/exception.py
@@ -1,16 +1,4 @@
 """Exceptions for the package."""
-import warnings
-
-
-class UserConfigValidationException(Exception):
-    """An exception indicating that some user configuration is not valid.
-
-    :param exception_message: A message describing the error.
-    :type exception_message: str
-    """
-    _error_code = "Invalid Configuration"
-    warnings.warn("UserConfigValidationException will be deprecated from dice_ml.utils. "
-                  "Please import UserConfigValidationException from raiutils.exceptions.")
 
 
 class SystemException(Exception):
