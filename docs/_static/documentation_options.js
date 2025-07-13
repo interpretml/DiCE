8a3aea404f857fa599bfa7da663d94823b674688
Amit Sharma <amit_sharma@live.com>
2025-07-13 23:00:34 +0530

    [Version Bump] Updated docs for v0.12 release (#461)
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    
    * updated docs
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    
    ---------
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
